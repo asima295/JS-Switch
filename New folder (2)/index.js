e767286b3d5435a2b5f62f9c7253bcb81e1418cb
asima295 <ftasleem581@gmail.com>
2024-11-02 13:19:35 +0500

    Add files via upload

---
+++ New folder (2)/index.js
@@ -0,0 +1,123 @@
+const color = ["red", "pink", "black","white","orange","pruple","grays","zink"];
+color [0] =["green"];
+color.push["blue"];
+console.log(color);
+console.log(typeof(Array))
+
+
+console.log(typeof(Array))
+
+
+let y="5";
+console.log(typeof(y))
+
+
+let x=5;
+console.log(typeof(x))
+
+
+let z="name";
+console.log(typeof(z))
+
+
+const person1={
+    name: "asma",
+    age: 17,
+    height: 5.0,
+}
+person1.height = 6.1;
+console.log(person1);
+console.log(typeof(Object))
+
+
+let a="5"+5;
+console.log(typeof(a))
+
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  let value = toCelsius(77);
+  document.getElementById("demo").innerHTML = value; 
+    
+
+  function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  let w = toCelsius(5);
+  document.getElementById("demo").innerHTML = w;
+
+
+
+
+  
+
+
+  let b = myFunction(4, 3);
+document.getElementById("p2").innerHTML = b;
+function myFunction(a, b) {
+  return a * b;
+}
+
+
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  let v = toCelsius(77);
+  document.getElementById("p1").innerHTML = v;
+  
+  let f = 16 + 4 + "Volvo";
+  console.log(f)
+
+
+
+
+  let c;      
+  c = 5;       
+  c = "John";
+  document.getElementById("p3").innerHTML = c;
+console.log(c)
+
+
+let carName1 = "Volvo XC60";
+document.getElementById("p4").innerHTML = carName1;
+
+let carName2 = 'Volvo XC60';
+document.getElementById("p5").innerHTML = carName2;
+
+
+
+let answer1 = "It's alright";
+document.getElementById("p6").innerHTML = answer1;
+
+
+let answer2 = "He is called 'Johnny'";
+document.getElementById("p7").innerHTML = answer2;
+
+
+let answer3 = 'He is called "Johnny"';
+document.getElementById("p8").innerHTML = answer3;
+
+
+
+
+let x1 = 34.00;
+document.getElementById("p9").innerHTML = x1;
+
+let x2 = 34;
+document.getElementById("p10").innerHTML =x2 ;
+
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+
+function to(a) {
+  return (5/9) * (a-32);
+}
+let val = to(77);
+document.getElementById("de").innerHTML = val;
